78d2696bd463b31bfdacef6d55099c0a7d8efc2b
pjqdyd <1909025079@qq.com>
2019-11-01 18:51:43 +0800

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+
+### Spring Cloud Security Oauth2 微服务安全认证授权
+
+#### 项目结构:
+
+```
+  ├─spring-cloud-security-oauth2-server    认证服务模块
+  ├─.gitignore                             .gitignore文件
+  ├─README.md                              README.md文件
+  └─pom.xml                                父模块pom文件
+ ```
+
+#### 简介:
+
+
+项目初始化....
---
+++ spring-cloud-security-oauth2-server/src/main/java/com/pjqdyd/SpringCloudSecurityOauth2ServerApplication.java
@@ -0,0 +1,19 @@
+package com.pjqdyd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**   
+ * @Description:  [Spring Cloud认证服务启动类]
+ * @Author:       pjqdyd
+ * @Version:      [v1.0.0]
+ */
+
+@SpringBootApplication
+public class SpringCloudSecurityOauth2ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudSecurityOauth2ServerApplication.class, args);
+    }
+
+}
